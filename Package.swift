319c8fb9f4252cd73f5b53c60eabdf4eb3bfc064
Pedro <pedro@pepicrft.me>
2024-08-04 18:43:08 +0200

    Use Swift Testing

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,6 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/tuist/Path", .upToNextMajor(from: "0.3.0")),
         .package(url: "https://github.com/apple/swift-log", .upToNextMajor(from: "1.6.1")),
+        .package(url: "https://github.com/swiftlang/swift-testing.git", branch: "main")
     ],
     targets: [
         .target(
@@ -32,6 +33,7 @@ let package = Package(
             name: "CommandTests",
             dependencies: [
                 "Command",
+                .product(name: "Testing", package: "swift-testing")
             ]
         ),
     ]
--- Project.swift
+++ Project.swift
@@ -33,6 +33,7 @@ let project = Project(name: "Command", targets: [
         ],
         dependencies: [
             .target(name: "Command"),
+            .external(name: "Testing")
         ]
     ),
 ])
