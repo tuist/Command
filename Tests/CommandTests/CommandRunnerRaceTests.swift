da80b277707f56092b147cec4d2918c74f83e5e3
Pedro <pedro@pepicrft.me>
2025-02-20 14:21:46 +0100

    Disable the tests on Windows

--- Tests/CommandTests/CommandRunnerRaceTests.swift
+++ Tests/CommandTests/CommandRunnerRaceTests.swift
@@ -2,24 +2,26 @@ import Mockable
 import Testing
 @testable import Command
 
-struct CommandRunnerRaceTests {
-    @Test func runsManyConcurrent_successfully() async throws {
-        #if os(Linux) || os(macOS)
-            let commandRunner = CommandRunner()
+#if !os(Linux)
+    struct CommandRunnerRaceTests {
+        @Test func runsManyConcurrent_successfully() async throws {
+            #if os(Linux) || os(macOS)
+                let commandRunner = CommandRunner()
 
-            try await withThrowingTaskGroup(of: String.self) { group in
-                for _ in 0 ..< 1000 {
-                    group.addTask {
-                        try await commandRunner
-                            .run(arguments: ["echo", "test"])
-                            .reduce("") { $0 + ($1.string() ?? "") }
+                try await withThrowingTaskGroup(of: String.self) { group in
+                    for _ in 0 ..< 1000 {
+                        group.addTask {
+                            try await commandRunner
+                                .run(arguments: ["echo", "test"])
+                                .reduce("") { $0 + ($1.string() ?? "") }
+                        }
+                    }
+
+                    for try await result in group {
+                        #expect(result == "test\n")
                     }
                 }
-
-                for try await result in group {
-                    #expect(result == "test\n")
-                }
-            }
-        #endif
+            #endif
+        }
     }
-}
+#endif
--- Tests/CommandTests/CommandRunnerTests.swift
+++ Tests/CommandTests/CommandRunnerTests.swift
@@ -2,71 +2,73 @@ import Mockable
 import Testing
 @testable import Command
 
-struct CommandRunnerTests {
-    @Test func runs_successfully() async throws {
-        // Given
-        let commandRunner = CommandRunner()
+#if !os(Linux)
+    struct CommandRunnerTests {
+        @Test func runs_successfully() async throws {
+            // Given
+            let commandRunner = CommandRunner()
 
-        // When
-        #if os(Windows)
-            let result = try await commandRunner.run(arguments: ["cmd.exe", "/c", "echo", "foo"])
-                .reduce(into: [String]()) { $0.append($1.string()) }
+            // When
+            #if os(Windows)
+                let result = try await commandRunner.run(arguments: ["cmd.exe", "/c", "echo", "foo"])
+                    .reduce(into: [String]()) { $0.append($1.string()) }
+                // Then
+                #expect(result == ["foo\r\n"])
+            #else
+                let result = try await commandRunner.run(arguments: ["echo", "foo"])
+                    .reduce(into: [String]()) { $0.append($1.string()) }
+                // Then
+                #expect(result == ["foo\n"])
+            #endif
+        }
+
+        @Test func lookupExecutable_withAbsolutePath() throws {
+            // Given
+            let commandRunner = CommandRunner()
+            #if os(Windows)
+                let absolutePath = "C:/Windows/System32/cmd.exe"
+            #else
+                let absolutePath = "/bin/echo"
+            #endif
+
+            // When
+            let executableURL = try commandRunner.lookupExecutable(firstArgument: absolutePath)
+
             // Then
-            #expect(result == ["foo\r\n"])
-        #else
-            let result = try await commandRunner.run(arguments: ["echo", "foo"])
-                .reduce(into: [String]()) { $0.append($1.string()) }
+            #expect(executableURL.path == absolutePath)
+        }
+
+        @Test func lookupExecutable_withRegularCommand() throws {
+            // Given
+            let commandRunner = CommandRunner()
+            #if os(Windows)
+                let command = "cmd.exe"
+            #else
+                let command = "echo"
+            #endif
+
+            // When
+            let executableURL = try commandRunner.lookupExecutable(firstArgument: command)
+
             // Then
-            #expect(result == ["foo\n"])
-        #endif
+            #expect(executableURL.path.hasSuffix("/\(command)") == true)
+        }
+
+        @Test func lookupExecutable_withInvalidCommand() throws {
+            // Given
+            let commandRunner = CommandRunner()
+            let command = "nonexistentcommand"
+
+            // When & Then
+            #expect(throws: (any Error).self, performing: { try commandRunner.lookupExecutable(firstArgument: command) })
+        }
+
+        @Test func lookupExecutable_withMissingExecutableCommand() throws {
+            // Given
+            let commandRunner = CommandRunner()
+
+            // When & Then
+            #expect(throws: (any Error).self, performing: { try commandRunner.lookupExecutable(firstArgument: nil) })
+        }
     }
-
-    @Test func lookupExecutable_withAbsolutePath() throws {
-        // Given
-        let commandRunner = CommandRunner()
-        #if os(Windows)
-            let absolutePath = "C:/Windows/System32/cmd.exe"
-        #else
-            let absolutePath = "/bin/echo"
-        #endif
-
-        // When
-        let executableURL = try commandRunner.lookupExecutable(firstArgument: absolutePath)
-
-        // Then
-        #expect(executableURL.path == absolutePath)
-    }
-
-    @Test func lookupExecutable_withRegularCommand() throws {
-        // Given
-        let commandRunner = CommandRunner()
-        #if os(Windows)
-            let command = "cmd.exe"
-        #else
-            let command = "echo"
-        #endif
-
-        // When
-        let executableURL = try commandRunner.lookupExecutable(firstArgument: command)
-
-        // Then
-        #expect(executableURL.path.hasSuffix("/\(command)") == true)
-    }
-
-    @Test func lookupExecutable_withInvalidCommand() throws {
-        // Given
-        let commandRunner = CommandRunner()
-        let command = "nonexistentcommand"
-
-        // When & Then
-        #expect(throws: (any Error).self, performing: { try commandRunner.lookupExecutable(firstArgument: command) })
-    }
-
-    @Test func lookupExecutable_withMissingExecutableCommand() throws {
-        // Given
-        let commandRunner = CommandRunner()
-
-        // When & Then
-        #expect(throws: (any Error).self, performing: { try commandRunner.lookupExecutable(firstArgument: nil) })
-    }
-}
+#endif
